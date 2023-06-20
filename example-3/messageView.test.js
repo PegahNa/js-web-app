44ff8607afcc3bb5d9be1927945b7efdb52e56b0
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-20 23:16:55 +0100

    message-input added to displayMessage

--- example-3/messageView.test.js
+++ example-3/messageView.test.js
@@ -6,16 +6,24 @@ const fs = require("fs");
 const MessageView = require("./messageView");
 
 describe("MessageView", () => {
-  it("clicks the button", () => {
+  it("clicks the button and displays the message", () => {
     // Arrange
     document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new MessageView();
     // Act
     const buttonEl = document.querySelector("#show-message-button");
+
+    const inputEl = document.querySelector("#message-input");
+
+    inputEl.value = "This is a test message";
+
     buttonEl.click();
     // Assert
     expect(document.querySelector("#message")).not.toBeNull();
+    expect(document.querySelector("#message").innerText).toEqual(
+      "This is a test message"
+    );
   });
   // show the hide message
   it("hides the message", () => {
