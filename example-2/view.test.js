21f7dd3209e4b1f2c8ada385b6dc2ffc6b8d7a9a
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-20 08:40:52 +0100

    view.test added

--- example-2/view.test.js
+++ example-2/view.test.js
@@ -2,15 +2,25 @@
  * @jest-environment jsdom
  */
 
-const fs = require('fs');
-const View = require('./view');
+const fs = require("fs");
+const View = require("./view");
 
-describe('Page view', () => {
-  it('displays 2 paragraphs', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+describe("Page view", () => {
+  it("displays 2 paragraphs", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new View();
 
-    expect(document.querySelectorAll('p').length).toBe(2);
+    expect(document.querySelectorAll("p").length).toBe(2);
   });
+
+  it("adds a new paragraph", () => {});
+  // Arrange
+  document.body.innerHTML = fs.readFileSync("./index.html");
+  const view = new View();
+  // Act
+  view.addParagraph();
+  view.addParagraph();
+  // Assert
+  expect(document.querySelectorAll("p").length).toBe(4);
 });
