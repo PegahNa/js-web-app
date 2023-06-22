46bb793a961cf9da12458e00ace74492204f29e3
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-22 18:25:34 +0100

    the loadNotes method with fetch added

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,6 +1,8 @@
+const NotesClient = require("./notesClient");
 const NotesView = require("./notesView.js");
 const NotesModel = require("./notesModel.js");
 
+const client = new NotesClient();
 const model = new NotesModel();
 const view = new NotesView(model);
 view.displayNotes();
