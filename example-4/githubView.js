147949a7716b4d43e9d41d2bdf7d7dbcca3fabbb
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-22 14:01:39 +0100

    example 4

---
+++ example-4/githubView.js
@@ -0,0 +1,21 @@
+class GithubView {
+  constructor(model, client) {
+    this.model = model;
+    this.client = client;
+
+    const submitButtonEl = document.querySelector("#submit-button");
+    const repoInputEl = document.querySelector("#repo-name-input");
+
+    submitButtonEl.addEventListener("click", () => {
+      const repoName = repoInputEl.value;
+
+      this.client.getRepoInfo(repoName, (repoData) => {
+        console.log(repoData);
+      });
+    });
+  }
+
+  display(repoData) {}
+}
+
+module.exports = GithubView;
